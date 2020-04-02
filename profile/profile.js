6b3f896f0024e49d0b74f63312d33ce280428a9a
kehinde <kehindesalaudeen222@gmail.com>
2020-04-02 02:44:17 +0100

    My profile page

---
+++ profile/profile.js
@@ -0,0 +1,13 @@
+
+      function play()
+      {      
+      var varry = document.getElementById('aside');
+      varry.style = "display:block";
+   
+      };
+      function clo()
+      {
+      var varry = document.getElementById('aside');
+      varry.style = "display:none";
+        
+      }
