ee1b077b93ed195d375722a5186f02dcff7e1167
kehinde <kehindesalaudeen222@gmail.com>
2020-06-08 17:35:30 +0100

    port

--- javascript/head.js
+++ javascript/head.js
@@ -3,6 +3,7 @@ head.innerHTML = `
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Digital evolution</title>
+<link rel="shortcut icon" href="../images/bus.jpg" type="image/x-icon">
 <link rel="stylesheet" href="../css/bootstrap.css">
 <link rel="stylesheet" href="../css/style.css">
 <link rel="stylesheet" href="../css/custom.css">
