5345645ca3325919c97810ff630d009c6d3565ad
kehinde <kehindesalaudeen222@gmail.com>
2020-06-11 01:01:36 +0100

    download enabled

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,21 +1,36 @@
 var gallery = document.getElementById('pics')
-for (let i = 1; i <= 102; i++) {
-    gallery.innerHTML +=`
-    <div class="col-lg-3 col-md-4 col-sm-12 h-auto"  >
-    <a href='../gallery/gallery (${i}).jpg'>
-
-        <img src="../gallery/gallery (${i}).jpg" alt="" width='100%' height='auto%' class='img card-body'>
-        </a>
-
-    </div>
-    `
+document.getElementById("back").style.display = "none"
+function start(params) {
+    for (let i = 1; i <= 102; i++) {
+        gallery.innerHTML +=`
+        <div class="col-lg-3 col-md-4 col-sm-12 h-auto"  >
+        <a href='#'>
+    
+            <img src="../gallery/gallery (${i}).jpg" alt="" width='100%' height='auto%' class='img card-body' onclick="img(event, ${i})">
+            </a>
+    
+        </div>
+        `
+    }
+    for (let i = 1; i <= 6; i++) {
+        gallery.innerHTML +=`
+        <div class="col-lg-3 col-md-4 col-sm-12 h-auto"  >
+    
+            <img src="../gallery/gallery (${i}).PNG" alt="" width='100%' height='auto%' class='img'>
+    
+        </div>
+        `
+    }
+    document.getElementById("super").style.display = "none"
+    document.getElementById("back").style.display = "none"
 }
-for (let i = 1; i <= 6; i++) {
-    gallery.innerHTML +=`
-    <div class="col-lg-3 col-md-4 col-sm-12 h-auto"  >
+start()
+var show = document.getElementsByClassName("img")
 
-        <img src="../gallery/gallery (${i}).PNG" alt="" width='100%' height='auto%' class='img'>
-
-    </div>
-    `
+function img(e, i) {
+    document.getElementById("back").style.display = "block"
+        document.getElementById("pics").innerHTML = `<div class='text-center container' id="super">
+        <img src="${e.target.src}" alt="" width='auto' height='400px' class='mt-5 col-lg-6 col-sm-12' /> <br>
+        <a href="${e.target.src}" download="img"> <button class="btn-success btn m-2">download</button></a></div>`
+    alert(i)
 }
